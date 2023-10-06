b93e9c5cc261675a52bab26acb80b7c71e4ab341
ueyudiud <ueyudiud@163.com>
2023-10-06 15:11:21 +0800

    Add __new__ function for int.

--- src/aintlib.c
+++ src/aintlib.c
@@ -14,8 +14,8 @@
 
 #include "alolib.h"
 
-static a_u32 int_new(a_henv env) {
-    Value v = api_elem(env, 0);
+static a_msg int_new(a_henv env) {
+    Value v = api_elem(env, 1); //TODO value checking.
     switch (v_get_tag(v)) {
         case T_NIL:
         case T_FALSE: {
@@ -34,12 +34,11 @@ static a_u32 int_new(a_henv env) {
             alo_pushint(env, cast(a_int, v_as_float(v)));
             break;
         }
-        case T_HSTR:
-        case T_ISTR: {
+        case T_STR: {
             a_int radix = aloL_optint(env, 1, 0);
             GStr* str = v_as_str(v);
             char const* p = str2ntstr(str);
-            char const* q;
+            char* q; /* strtol() says end pointer should be mutable. */
 
             if (isspace(p[0]))
                 goto einval;
@@ -62,8 +61,7 @@ static a_u32 int_new(a_henv env) {
             break;
         }
         default: {
-            alo_typeof(env, 0);
-            break;
+            aloL_raisef(env, "%s value cannot convert to int.", v_nameof(env, v)); //TODO __int__ meta call.
         }
     }
     return 1;
@@ -71,7 +69,7 @@ static a_u32 int_new(a_henv env) {
 
 void aloopen_int(a_henv env) {
 	static aloL_Entry const bindings[] = {
-        { "__call__", int_new }
+        { "__new__", int_new }
 	};
 
     v_set_obj(env, api_incr_stack(env), &G(env)->_types._int);
