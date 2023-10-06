e69b31ed591d2a537042061dfb5e31f3dd6dde28
ueyudiud <ueyudiud@163.com>
2023-10-06 15:33:18 +0800

    Fix float conversion bug.

--- src/aintlib.c
+++ src/aintlib.c
@@ -16,7 +16,10 @@
 
 static a_msg int_new(a_henv env) {
     Value v = api_elem(env, 1); //TODO value checking.
-    switch (v_get_tag(v)) {
+    if (v_is_float(v)) {
+        alo_pushint(env, cast(a_int, v_as_float(v)));
+    }
+    else switch (v_get_tag(v)) {
         case T_NIL:
         case T_FALSE: {
             alo_pushint(env, 0);
@@ -27,15 +30,11 @@ static a_msg int_new(a_henv env) {
             break;
         }
         case T_INT: {
-            alo_settop(env, 1);
-            break;
-        }
-        case T_FLOAT: {
-            alo_pushint(env, cast(a_int, v_as_float(v)));
+            alo_settop(env, 2);
             break;
         }
         case T_STR: {
-            a_int radix = aloL_optint(env, 1, 0);
+            a_int radix = aloL_optint(env, 2, 0);
             GStr* str = v_as_str(v);
             char const* p = str2ntstr(str);
             char* q; /* strtol() says end pointer should be mutable. */
@@ -45,7 +44,7 @@ static a_msg int_new(a_henv env) {
 
             errno = 0;
             a_int i = strtol(p, &q, radix);
-            if (p == q)
+            if (q != p + str->_len)
                 goto einval;
             if (errno == ERANGE)
                 goto erange;
@@ -53,15 +52,15 @@ static a_msg int_new(a_henv env) {
             break;
 
         einval:
-            aloL_argerror(env, 0, "not number value.");
+            aloL_argerror(env, 1, "cannot parse string to integer.");
             break;
 
         erange:
-            aloL_argerror(env, 0, "integer out of range.");
+            aloL_argerror(env, 1, "integer out of range.");
             break;
         }
         default: {
-            aloL_raisef(env, "%s value cannot convert to int.", v_nameof(env, v)); //TODO __int__ meta call.
+            aloL_argerror(env, 1, "cannot convert to int."); //TODO __int__ meta call.
         }
     }
     return 1;
