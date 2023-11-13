5898838933e24af57185bdcef706f6e5d5b5fd24
ueyudiud <ueyudiud@163.com>
2023-11-13 11:04:56 +0800

    Add int.ror and int.rol function.

--- src/aintlib.c
+++ src/aintlib.c
@@ -7,78 +7,113 @@
 
 #include <stdlib.h>
 
-#include "atable.h"
 #include "agc.h"
-#include "avm.h"
 #include "aapi.h"
 
 #include "alolib.h"
 
-static a_msg int_new(a_henv env) {
+#define l_pushuint(env,v) alo_pushint(env, cast(a_int, v))
+#define l_checkuint(env,id) cast(a_uint, aloL_checkint(env, id))
+
+#define INT_BITS (sizeof(a_int) * CHAR_BIT)
+
+static a_msg int___new__(a_henv env) {
     if (alo_tagof(env, 0) == ALO_EEMPTY) {
         alo_pushint(env, 0);
-        return 1;
     }
+    else {
+        Value v = api_elem(env, 0);
+        if (v_is_float(v)) {
+            alo_pushint(env, cast(a_int, v_as_float(v)));
+        }
+        else switch (v_get_tag(v)) {
+                case T_FALSE: {
+                    alo_pushint(env, 0);
+                    break;
+                }
+                case T_TRUE: {
+                    alo_pushint(env, 1);
+                    break;
+                }
+                case T_INT: {
+                    alo_settop(env, 1);
+                    break;
+                }
+                case T_STR: {
+                    a_int radix = aloL_optint(env, 1, 0);
+                    GStr* str = v_as_str(v);
+                    char const* p = str2ntstr(str);
+                    char* q; /* strtol() says end pointer should be mutable. */
 
-    Value v = api_elem(env, 0);
-    if (v_is_float(v)) {
-        alo_pushint(env, cast(a_int, v_as_float(v)));
+                    if (isspace(p[0]))
+                        goto einval;
+
+                    int old_err = errno;
+                    errno = 0;
+                    a_int i = strtol(p, &q, radix);
+                    int the_err = errno;
+                    errno = old_err;
+
+                    if (q != p + str->_len)
+                        goto einval;
+                    if (the_err == ERANGE)
+                        goto erange;
+                    alo_pushint(env, i);
+                    break;
+
+                einval:
+                    aloL_argerror(env, 0, "cannot parse string to integer.");
+                    break;
+
+                erange:
+                    aloL_argerror(env, 0, "integer out of range.");
+                    break;
+                }
+                default: {
+                    if (aloL_gettm(env, 0, "__int__") == ALO_EEMPTY) {
+                        aloL_argerror(env, 0, "cannot convert to int.");
+                    }
+                    a_isize n = alo_insert(env, 0);
+                    alo_call(env, n - 1, 1);
+                    break;
+                }
+            }
     }
-    else switch (v_get_tag(v)) {
-        case T_FALSE: {
-            alo_pushint(env, 0);
-            break;
-        }
-        case T_TRUE: {
-            alo_pushint(env, 1);
-            break;
-        }
-        case T_INT: {
-            alo_settop(env, 1);
-            break;
-        }
-        case T_STR: {
-            a_int radix = aloL_optint(env, 1, 0);
-            GStr* str = v_as_str(v);
-            char const* p = str2ntstr(str);
-            char* q; /* strtol() says end pointer should be mutable. */
+    return 1;
+}
 
-            if (isspace(p[0]))
-                goto einval;
+static a_msg int_rol(a_henv env) {
+    a_uint i = l_checkuint(env, 0);
+    a_int n = aloL_checkint(env, 1);
 
-            int old_err = errno;
-            errno = 0;
-            a_int i = strtol(p, &q, radix);
-            int the_err = errno;
-            errno = old_err;
+    n &= INT_BITS - 1;
 
-            if (q != p + str->_len)
-                goto einval;
-            if (the_err == ERANGE)
-                goto erange;
-            alo_pushint(env, i);
-            break;
+    a_uint o = (i << n) | (i >> (INT_BITS - n));
 
-        einval:
-            aloL_argerror(env, 0, "cannot parse string to integer.");
-            break;
+    l_pushuint(env, o);
+    return 1;
+}
 
-        erange:
-            aloL_argerror(env, 0, "integer out of range.");
-            break;
-        }
-        default: {
-            aloL_argerror(env, 0, "cannot convert to int."); //TODO __int__ meta call.
-        }
-    }
+static a_msg int_ror(a_henv env) {
+    a_uint i = l_checkuint(env, 0);
+    a_int n = aloL_checkint(env, 1);
+
+    n &= INT_BITS - 1;
+
+    a_uint o = (i >> n) | (i << (INT_BITS - n));
+
+    l_pushuint(env, o);
     return 1;
 }
 
 void aloopen_int(a_henv env) {
 	static aloL_Entry const bindings[] = {
-        { "__new__", int_new },
+        { "rol", int_rol },
+        { "ror", int_ror },
+        { "__new__", int___new__ },
+        { "__look__", null },
         { "MAX", null },
-        { "MIN", null },
+        { "MIN", null }
 	};
 
     alo_pushptype(env, ALO_TINT);
@@ -89,6 +124,9 @@ void aloopen_int(a_henv env) {
 
     alo_pushint(env, INT32_MIN);
     aloL_puts(env, -2, "MIN");
+
+    alo_push(env, -1);
+    aloL_puts(env, -2, "__look__");
 
     ai_gc_trigger(env);
 }
